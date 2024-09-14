090eda49024862270f8a063f16d628372f004e29
neelareddy <neelareddy.i0204@gmail.com>
2024-09-14 10:46:09 +0530

    script

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,30 +1,43 @@
+# #!/bin/bash
+
+# USERID=$(id -u)
+
+
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo "$2...FAILURE"
+#         exit 1
+#     else
+#         echo "$2...SUCCESS"
+#     fi
+# }
+
+# if [ $USERID -ne 0 ]
+# then
+#     echo "Please run this script with root access."
+#     exit 1 # manually exit if error comes.
+# else
+#     echo "You are super user."
+# fi
+
+# dnf install mysql -y
+# VALIDATE $? "Installing MySQL"
+
+# dnf install git -y
+# VALIDATE $? "Installing git"
+
+# dnf install nginx -y
+# VALIDATE $? "Installing nginx"
+
 #!/bin/bash
 
-USERID=$(id -u)
+# Capture the output of a command
+free_space=$(df / | grep / | awk '{ print $4 }')
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo "$2...FAILURE"
-        exit 1
-    else
-        echo "$2...SUCCESS"
-    fi
-}
-
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+# Use the variable in an if statement
+if [ $free_space -lt 1000000 ]; then
+    echo "Low disk space!"
 else
-    echo "You are super user."
+    echo "Sufficient disk space!"
 fi
-
-dnf install mysql -y
-VALIDATE $? "Installing MySQL"
-
-dnf install git -y
-VALIDATE $? "Installing git"
-
-dnf install nginx -y
-VALIDATE $? "Installing nginx"
