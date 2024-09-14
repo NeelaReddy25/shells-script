153c71a0f2f13a077b8333e189c208afd4291ade
neelareddy <neelareddy.i0204@gmail.com>
2024-09-14 11:17:35 +0530

    script

--- 11-functions.sh
+++ 11-functions.sh
@@ -4,14 +4,18 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2...FAILURE"
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo "$2...SUCCESS"
+        echo -e "$2...$G SUCCESS $N"
     fi
 }
 
@@ -30,7 +34,10 @@ dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing git"
 
 dnf install nginx -y &>>$LOGFILE
-VALIDATE $? "Installing nginx"
+VALIDATE $? "Installing Nginx"
+
+dnf install Docker -y &>>$LOGFILE
+VALIDATE $? "Installing Docker"
 
 # #!/bin/bash
 
