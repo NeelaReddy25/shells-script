0c40a69a36199deaa811539ae4e83a80987277fc
neelareddy <neelareddy.i0204@gmail.com>
2024-09-15 18:22:20 +0530

    good effects

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COURSE="DevOps from currentscript"
+COURSE="DevOps from current script"
 
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
