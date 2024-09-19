945355775525ab5ef580c2a2bd4563fbe647b6a1
neelareddy <neelareddy.i0204@gmail.com>
2024-09-19 16:59:32 +0530

    script

--- 17-current-script.sh
+++ 17-current-script.sh
@@ -5,6 +5,6 @@ COURSE="DevOps from current script"
 echo "Before calling other script, Course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-#./18-other-script.sh
+./18-other-script.sh
 
 echo "After calling other script, Course: $COURSE"
