4cf8898cacf857d8750c6a02ce4c496692f6909b
neelareddy <neelareddy.i0204@gmail.com>
2024-09-20 19:46:59 +0530

    devops

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -2,6 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THERSHOLD=6
+MESSAGE=""
 
 while IFS= read -r line
 do
@@ -9,6 +10,8 @@ do
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THERSHOLD ]
     then
-        echo "$FOLDER is more than $DISK_THERSHOLD, current usage: $USAGE"
+        MESSAGE="$FOLDER is more than $DISK_THERSHOLD, current usage: $USAGE"
     fi
 done <<< $DISK_USAGE
+
+echo "Message: $MESSAGE"
