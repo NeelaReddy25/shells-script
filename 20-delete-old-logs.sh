6be437663d03b9fc81f53252339612b1949624ad
neelareddy <neelareddy.i0204@gmail.com>
2024-09-20 15:59:35 +0530

    script

---
+++ 20-delete-old-logs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+    echo -e "$G Sourcedirectory exists $N"
+    exit 1
+fi
