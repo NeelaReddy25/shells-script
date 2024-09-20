e7a0ef5c310005c00773cadb7cf51ff9124acaf8
neelareddy <neelareddy.i0204@gmail.com>
2024-09-20 16:40:03 +0530

    script

--- 20-delete-old-logs.sh
+++ 20-delete-old-logs.sh
@@ -9,6 +9,12 @@ N="\e[0m"
 
 if [ -d $SOURCE_DIRECTORY ]
 then
-    echo -e "$G Sourcedirectory exists $N"
+    echo -e "$G Source directory exists $N"
+else
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
     exit 1
 fi
+
+FILES=$(find $SOURCE_DIRECTORY -name "*log" -mtime +14)
+
+echo "Files to delete: $FILES"
